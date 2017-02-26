3c0e8b0c8f08fe1e42ae495b1fe5955aa8a16b03
Michael Crivello <macrivel@calpoly.edu>
2017-02-26 15:02:29 -0800

    remove reference to unused repository class

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -1,13 +1,8 @@
 package base;
 
-import base.data.CourseRepository;
-import base.data.InMemoryCourseRepositoy;
-import base.data.InMemoryPostRepository;
-import base.data.PostRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
