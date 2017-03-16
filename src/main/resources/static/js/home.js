aa04bfdd2f901c4336d9c58c94d50897729ad07e
Michael Crivello <macrivel@calpoly.edu>
2017-03-15 18:57:23 -0700

    moving home js to separate file, adding signout functionality

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,68 @@
+$(function(){
+    var signOutButton = $("#signOutButton");
+    var userInfo = $("#userInfo");
+    var addCourseButton1 = $("#addCourse1");
+
+    addCourseButton1.click(function (e) {
+        addCourse1()
+    });
+
+    signOutButton.click(function () {
+        $.ajax({
+            type: "GET",
+            url: "/signout"
+        }).done(function (data, textStatus, jqXHR) {
+            console.log("Logged out of PolyPaths");
+            $(location).attr('href', '/login'); //redirect to login page
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR.status);
+            if (jqXHR.status == 401) {
+                $(location).attr('href', '/login'); //redirect to login page
+            }
+        });
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "/users/me",
+        contentType:"application/json",
+        dataType: "json"
+    }).done(function (data, textStatus, jqXHR) {
+        console.log(data);
+        userInfo.text(data.firstName + " " + data.lastName + " (" + data.roles[0] + ")");
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR.status);
+        if (jqXHR.status == 401) {
+            $(location).attr('href', '/login'); //redirect to login page
+        }
+    });
+
+    function addCourse1() {
+        $("#quarter1").append('<div class="course">test</div>');
+    }
+
+    function addCourse(e) {
+        var el = document.createElement("div");//.setAttribute("class", "course");
+        el.className = "course";
+        el.innerHTML = "Math 248";
+        e.before(el);
+        getCourse(164);
+    }
+
+    function getCourse(c) {
+        $.ajax({
+            type: "GET",
+            url: "/courses/" + c,
+            contentType: "application/json",
+            dataType: "json"
+        }).done(function (data, textStatus, jqXHR) {
+            console.log(data);
+            console.log("finished AJAX query");
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR.status);
+            if (jqXHR.status == 401) {
+                $(location).attr('href', '/login'); //redirect to login page
+            }
+        });
+    }
+});
